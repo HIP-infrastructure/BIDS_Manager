30d6b06006d0a41b64de1b2a69b4b1478f491c54
Nicolas Roehri <nicolas.roehri@etu.univ-amu.fr>
2018-08-15 15:41:29 +0200

    finished name changing method for vhdr files

--- brainvision_hdr.py
+++ brainvision_hdr.py
@@ -6,8 +6,8 @@ class BrainvisionHeader(object):
 
     hdr_ext = '.vhdr'
     data_tag = '[Channel Infos]'
-    expr = r'Ch\d{1,3}=(?P<label>.*?)(?P<num>\d{0,3}),'
-    expr2besub = [r'Ch\d{1,3}=(?P<label>', r')(?P<num>\d{0,3}),']
+    expr = r'(?P<channel>Ch\d{1,3}=)(?P<label>.*?)(?P<num>\d{0,3}),'
+    expr2besub = [r'(?P<channel>Ch\d{1,3}=)(?P<label>', r')(?P<num>\d{0,3}),']
     expr2sub = r''
 
     def __init__(self, filename=None):
@@ -18,18 +18,16 @@ class BrainvisionHeader(object):
         self.data_section = []
         self.filename = filename
         if self.filename:
-            self.read_header(self.filename)
+            self.read_header()
 
-    def read_header(self, filename):
-        if os.path.isfile(filename) and os.path.splitext(filename)[1] == self.hdr_ext:
-            if not self.filename == filename:
-                self.filename = filename
+    def read_header(self):
+        if os.path.isfile(self.filename) and os.path.splitext(self.filename)[1] == self.hdr_ext:
             with open(self.filename, 'r') as file:
                 for line in file:
                     self.header.append(line)
             self.fill_data_section()
         else:
-            raise FileNotFoundError(filename + ' was not found.')
+            raise FileNotFoundError(self.filename + ' was not found.')
 
     def fill_data_section(self):
         data_sect_starts = self.header.index(self.data_tag + '\n') + 1
@@ -37,6 +35,10 @@ class BrainvisionHeader(object):
         self.data_section = self.header[data_sect_starts:data_sect_ends]
         """search for expression that starts with 'Ch', is followed by one to three digits and '=' and finishes with a 
         comma. The first group is alphanumeric and the second can be made of  three digits"""
+        self.channel_list.clear()
+        self.electrode_list.clear()
+        self.electrode_set.clear()
+
         for line in self.data_section:
             m = re.search(self.expr, line)
             self.channel_list.append(m['label'] + m['num'])
@@ -44,17 +46,30 @@ class BrainvisionHeader(object):
         self.electrode_set = set(self.electrode_list)
 
     def modify_header(self, orig_name, new_name):
+        def repl(m):
+            return m['channel'] + new_name + m['num']
+
         if orig_name not in self.electrode_set:
             raise NameError(orig_name + ' electrode is not found in ' + self.filename + '.')
-        # for line in self.header:
-        #     re.sub(self.expr2besub[0] + orig_name + self.expr2besub[1], r'\g<>', "abcdef")
-        ' ab cdef'
+        for cnt, line in enumerate( self.header):
+            line = re.sub(self.expr2besub[0] + orig_name + self.expr2besub[1], repl, line)
+            self.header[cnt] = line
+        self.fill_data_section()
+
+    def write_header(self):
+        if os.path.isfile(self.filename) and os.path.splitext(self.filename)[1] == self.hdr_ext:
+            # with open(self.filename.replace('.vhdr', '_test.vhdr'), 'w') as file:
+            with open(self.filename, 'w') as file:
+                for line in self.header:
+                    file.write(line)
 
 
 if __name__ == '__main__':
-    bv_path = 'D:/roehri/BIDs/2048/sub-PaiJul/ses-01/ieeg/sub-PaiJul_ses-01_task-seizure_run-01_ieeg.vhdr'
-    bv_hdr = BrainvisionHeader()
-    bv_hdr.read_header(bv_path)
+    bv_path = r'F:\Users\Nico\Documents\Marseille\PHRC\small_2048_4test\sub-PaiJul\ses-01\ieeg\sub-PaiJul_ses-01_task-seizure_run-01_ieeg.vhdr'
+    bv_hdr = BrainvisionHeader(bv_path)
     print(bv_hdr.electrode_set)
-    bv_hdr.modify_header("TP'", 'WWW')
-
+    old_name = bv_hdr.electrode_list[0]
+    bv_hdr.modify_header(old_name, 'WWW')
+    print(bv_hdr.electrode_set)
+    bv_hdr.modify_header("WWW", old_name)
+    # bv_hdr.write_header()
