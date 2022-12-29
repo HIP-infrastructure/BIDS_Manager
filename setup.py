e92897430ba21338338f7bbb6a03a78e3451f4be
Sebastien Tourbier <sebastien.tourbier1@gmail.com>
2022-12-29 15:56:46 +0100

    maint: update version to 0.3.3-dev

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ here = path.abspath(path.dirname(__file__))
 
 setup(
     name='bids_manager',
-    version='0.3.2',
+    version="0.3.3-dev",
     zip_safe=False,
     #modifier l'url
     url='https://github.com/Dynamap/BIDS_Manager',
